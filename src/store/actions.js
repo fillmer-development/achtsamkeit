ea3e126d3363d5640e87421dc9f2bf9a0ea861c1
= <=>
2020-02-21 19:27:23 +0100

    Updates

--- src/store/actions.js
+++
@@ -1,5 +0,0 @@
-import { ACTIONS } from "./constants";
-
-export const setMoment = text => ({ type: ACTIONS.SET_MOMENT, text })
-
-export const setNote = text => ({ type: ACTIONS.SET_NOTE, text })
--- src/store/constants.js
+++
@@ -1,8 +0,0 @@
-export const ACTIONS = {
-    SET_MOMENT: "SET_MOMENT",
-    SET_NOTE: "SET_NOTE"
-}
-
-export const CONFIG = {
-    LOCALSTORAGE: "achtsamkeit-store"
-}
