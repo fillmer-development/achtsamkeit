a1fc52e482874fb6b3b5efdd145f29814d179099
= <=>
2020-09-15 16:38:52 +0200

    adjust styling

--- src/App.js
+++ src/App.js
@@ -3,23 +3,13 @@ import { connect } from 'react-redux';
 import DateSelector from './DateSelector';
 import Moments from './Moment';
 import Note from './Note';
+import Productivity from './Productivity';
 import { updateJournal } from './store/journal';
 import './style.scss';
-import RangeInput from './RangeInput';
-import { mapProductivityToHSL } from './utils';
-import { useEffect } from 'react';
-import Productivity from './Productivity';
 
 const App = ({ timestamp, entry = {}, updateFactory = f => f }) => {
   const update = updateFactory(timestamp)
-  useEffect(() => {
-    const stylesheet = document.styleSheets[0]
-    stylesheet.removeRule(0)
-    stylesheet.addRule(
-      'body',
-      `background-color: ${mapProductivityToHSL(entry.productivity)}`,
-      0)
-  }, [entry.productivity])
+
   return (
     <div className="app">
       <DateSelector />
@@ -27,13 +17,13 @@ const App = ({ timestamp, entry = {}, updateFactory = f => f }) => {
         value={entry.moment || ""}
         setValue={moment => update({ ...entry, moment })}
       />
-      <Note
-        value={entry.note || ""}
-        setValue={note => update({ ...entry, note })}
-      />
       <Productivity
         value={entry.productivity || ""}
         setValue={productivity => update({ ...entry, productivity })}
+      />
+      <Note
+        value={entry.note || ""}
+        setValue={note => update({ ...entry, note })}
       />
     </div>
   );
--- src/Productivity.js
+++ src/Productivity.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import RangeInput from './RangeInput'
+import { mapProductivityToHSL } from './utils'
 
 const Productivity = ({ value = 50, ...props }) => {
     return (
-        <div className='productivity'>
+        <div
+            className='productivity'
+            style={{ backgroundColor: mapProductivityToHSL(value) }}>
             <h2>Produktivität</h2>
             <RangeInput value={value} {...props} />
+            <h3>{value}</h3>
         </div>
     )
 }
